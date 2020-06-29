Fix error with rotation around z-axis

albumentations/augmentations/functional.py
@@ -2049,11 +2049,11 @@ def gaze_vector_as_is(gaze_vector, **params):
 
 
 def gaze_vector_hflip(gaze_vector, **params):
-    return gaze_vector_rotate(gaze_vector, 180, 0)
+    return gaze_vector_rotate(gaze_vector, x_angle=180)
 
 
 def gaze_vector_vflip(gaze_vector, **params):
-    return gaze_vector_rotate(gaze_vector, 0, 180)
+    return gaze_vector_rotate(gaze_vector, y_angle=180)
 
 
 def gaze_vector_flip(gaze_vector, d, **params):
@@ -2069,9 +2069,8 @@ def gaze_vector_flip(gaze_vector, d, **params):
     return gaze_vector
 
 
-def gaze_vector_rotate(gaze_vector, x_angle, y_angle):
-    print("gaze_vector_rotate", gaze_vector)
-    rotation_matrix = gaze_vector_rotation_matrix(x_angle, -1*y_angle)
+def gaze_vector_rotate(gaze_vector, x_angle=0, y_angle=0, z_angle=0):
+    rotation_matrix = gaze_vector_rotation_matrix(x_angle, y_angle, z_angle)
     np_gaze_vector = np.array(gaze_vector[:3])
     x, y, z = rotation_matrix.dot(np_gaze_vector)[:3]
     return x, y, abs(z)

albumentations/augmentations/gaze_vector_utils.py
@@ -97,7 +97,7 @@ def convert_gaze_vector_from_albumentations(
     return gaze_vector
 
 
-def gaze_vector_rotation_matrix(x_angle, y_angle):
+def gaze_vector_rotation_matrix(x_angle, y_angle, z_angle):
     x_angle_rad = math.radians(x_angle)
     cos_x = math.cos(x_angle_rad)
     sin_x = math.sin(x_angle_rad)
@@ -106,7 +106,12 @@ def gaze_vector_rotation_matrix(x_angle, y_angle):
     cos_y = math.cos(y_angle_rad)
     sin_y = math.sin(y_angle_rad)
 
+    z_angle_rad = math.radians(z_angle)
+    cos_z = math.cos(z_angle_rad)
+    sin_z = math.sin(z_angle_rad)
+
     x_rotation_matrix = np.array([[1, 0, 0], [0, cos_x, -1*sin_x], [0, sin_x, cos_x]])
     y_rotation_matrix = np.array([[cos_y, 0, sin_y], [0, 1, 0], [-sin_y, 0, cos_y]])
+    z_rotation_matrix = np.array([[cos_z, -1*sin_z, 0], [sin_z, cos_z, 0], [0, 0, 1]])
 
-    return x_rotation_matrix.dot(y_rotation_matrix)
+    return x_rotation_matrix.dot(y_rotation_matrix.dot(z_rotation_matrix))

albumentations/augmentations/transforms.py
@@ -553,7 +553,7 @@ class Rotate(DualTransform):
         return F.keypoint_rotate(keypoint, angle, **params)
 
     def apply_to_gaze_vector(self, gaze_vector, angle=0, **params):
-        return F.gaze_vector_rotate(gaze_vector, 0, angle)
+        return F.gaze_vector_rotate(gaze_vector, z_angle=angle)
 
     def get_transform_init_args_names(self):
         return ("limit", "interpolation", "border_mode", "value", "mask_value")
@@ -671,7 +671,7 @@ class ShiftScaleRotate(DualTransform):
         return F.bbox_shift_scale_rotate(bbox, angle, scale, dx, dy, **params)
 
     def apply_to_gaze_vector(self, gaze_vector, angle=0, **params):
-        return F.gaze_vector_rotate(gaze_vector, 0, angle)
+        return F.gaze_vector_rotate(gaze_vector, z_angle=angle)
 
     def get_transform_init_args(self):
         return {